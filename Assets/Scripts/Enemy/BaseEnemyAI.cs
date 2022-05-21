9b3626fc684e54277edea4a1f4ea7e38640fd400
Raestkjoot <gunnarmagnum@gmail.com>
2022-05-21 16:30:55 +0200

    Planning the enemy AI

---
+++ Assets/Scripts/Enemy/BaseEnemyAI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BaseEnemyAI : MonoBehaviour
+{
+    private float attackRange;
+    private float attackSpeed;
+    private float attackDamage;
+
+    private float stopAtDist;
+    private float currentDist;
+
+    private void Update()
+    {
+        // calculate dist to player
+        // calculate angle to player
+        
+        // lerp angle towards 0
+        
+        // if dist > stopAtDist then
+        //   move towards player
+
+        // if dist <= attackRange then
+        //   Attack();
+    }
+}
